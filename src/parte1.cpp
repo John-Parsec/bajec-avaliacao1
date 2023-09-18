0d57174cf304ad830d820ce0eb2e39b4e0e7625c
albertolunia <albertolunia@gmail.com>
2023-09-18 10:09:29 -0300

    Parte 2 versão 1

--- src/parte1.cpp
+++ src/parte1.cpp
@@ -11,54 +11,128 @@ struct Passageiro{
     int NumAutorizacao;
 };
 
+struct Roteiro{
+    int Codigo;
+    string Data_hora;
+    string Duracao;
+    string Origem;
+    string Destino;
+};
 
+void MenuPassageiro();
+void MenuRoteiro();
 void IncluirPassageiro(vector<Passageiro> &passageiros);
 void ExcluirPassageiro(vector<Passageiro> &passageiros);
 void AlterarPassageiro(vector<Passageiro> &passageiros);
 void ListarPassageiro(vector<Passageiro> &passageiros);
 void LocalizarPassageiro(vector<Passageiro> &passageiros);
+void IncluirRoteiro(vector<Roteiro> &roteiros);
+void ExcluirRoteiro(vector<Roteiro> &roteiros);
+void AlterarRoteiro(vector<Roteiro> &roteiros);
+void ListarRoteiro(vector<Roteiro> &roteiros);
+void LocalizarRoteiro(vector<Roteiro> &roteiros);
 
 int main(){
-
     vector<Passageiro> passageiros;
-
-    int opcao = 0;
-
-    do {
-        cout << "1 - Incluir Passageiro" << endl;
-        cout << "2 - Excluir Passageiro" << endl;
-        cout << "3 - Alterar Passageiro" << endl;
-        cout << "4 - Listar Passageiro" << endl;
-        cout << "5 - Localizar Passageiro" << endl;
-        cout << "0 - Sair" << endl;
+    vector<Roteiro> roteiros;
+    int opcaoPrincipal = 0, opcao = 0;
+    
+    while(opcaoPrincipal != 3){
+        cout << "1 - Passageiro" << endl;
+        cout << "2 - Roteiro" << endl;
+        cout << "3 - Sair" << endl;
         cout << "Digite a opcao desejada: ";
-        cin >> opcao;
+        cin >> opcaoPrincipal;
+        cout << endl;
 
-        switch(opcao){
-            case 0:
-                cout << "Saindo..." << endl;
-                break;
+        switch(opcaoPrincipal){
             case 1:
-                IncluirPassageiro(passageiros);
+                do{
+                    MenuPassageiro();
+                    cin >> opcao;
+                    cout << endl;
+
+                    switch(opcao){
+                        case 1:
+                            IncluirPassageiro(passageiros);
+                            break;
+                        case 2:
+                            ExcluirPassageiro(passageiros);
+                            break;
+                        case 3:
+                            AlterarPassageiro(passageiros);
+                            break;
+                        case 4:
+                            ListarPassageiro(passageiros);
+                            break;
+                        case 5:
+                            LocalizarPassageiro(passageiros);
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            cout << "Opcao invalida!" << endl;
+                            break;
+                    }
+                } while(opcao != 0);
                 break;
             case 2:
-                ExcluirPassageiro(passageiros);
+                do{
+                    MenuRoteiro();
+                    cin >> opcao;
+                    cout << endl;
+
+                    switch(opcao){
+                        case 1:
+                            IncluirRoteiro(roteiros);
+                            break;
+                        case 2:
+                            ExcluirRoteiro(roteiros);
+                            break;
+                        case 3:
+                            AlterarRoteiro(roteiros);
+                            break;
+                        case 4:
+                            ListarRoteiro(roteiros);
+                            break;
+                        case 5:
+                            LocalizarRoteiro(roteiros);
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            cout << "Opcao invalida!" << endl;
+                            break;
+                    }
+                }while(opcao != 0);
                 break;
             case 3:
-                AlterarPassageiro(passageiros);
-                break;
-            case 4:
-                ListarPassageiro(passageiros);
-                break;
-            case 5:
-                LocalizarPassageiro(passageiros);
                 break;
             default:
                 cout << "Opcao invalida!" << endl;
                 break;
         }
-    }while(opcao != 0);
-    return 0;
+    }
+}
+
+void MenuPassageiro(){
+    cout << "1 - Incluir Passageiro" << endl;
+    cout << "2 - Excluir Passageiro" << endl;
+    cout << "3 - Alterar Passageiro" << endl;
+    cout << "4 - Listar Passageiro" << endl;
+    cout << "5 - Localizar Passageiro" << endl;
+    cout << "0 - Sair" << endl;
+    cout << "Digite a opcao desejada: ";
+}
+
+void MenuRoteiro(){
+    cout << "1 - Incluir Roteiro" << endl;
+    cout << "2 - Excluir Roteiro" << endl;
+    cout << "3 - Alterar Roteiro" << endl;
+    cout << "4 - Listar Roteiro" << endl;
+    cout << "5 - Localizar Roteiro" << endl;
+    cout << "0 - Sair" << endl;
+    cout << "Digite a opcao desejada: ";
 }
 
 void IncluirPassageiro(vector<Passageiro> &passageiros){
@@ -194,3 +268,149 @@ void LocalizarPassageiro(vector<Passageiro> &passageiros){
     }
     cout << "Passageiro nao encontrado!" << endl;
 }
+
+void IncluirRoteiro(vector<Roteiro> &roteiros){
+    int codigo;
+    Roteiro roteiro;
+
+    cout << "Digite o Codigo: ";
+    cin >> codigo;
+
+    if(roteiros.size() == 0){
+        roteiro.Codigo = codigo;
+        cout << "Digite a Data e Hora: ";
+        cin >> roteiro.Data_hora;
+        cout << "Digite a Duracao: ";
+        cin >> roteiro.Duracao;
+        cout << "Digite a Origem: ";
+        cin >> roteiro.Origem;
+        cout << "Digite o Destino: ";
+        cin >> roteiro.Destino;
+        roteiros.push_back(roteiro);
+        cout << "Roteiro incluido com sucesso!" << endl;
+        return;
+    }
+    else{
+        for(int i = 0; i < roteiros.size(); i++){
+            if(roteiros[i].Codigo == codigo){
+                cout << "Roteiro ja cadastrado!" << endl;
+                return;
+            }
+            roteiro.Codigo = codigo;
+            cout << "Digite a Data e Hora: ";
+            cin >> roteiro.Data_hora;
+            cout << "Digite a Duracao: ";
+            cin >> roteiro.Duracao;
+            cout << "Digite a Origem: ";
+            cin >> roteiro.Origem;
+            cout << "Digite o Destino: ";
+            cin >> roteiro.Destino;
+            roteiros.push_back(roteiro);
+            cout << "Roteiro incluido com sucesso!" << endl;
+            return;
+        }
+    }
+}
+
+void ExcluirRoteiro(vector<Roteiro> &roteiros){
+    int codigo;
+    cout << "Digite o Codigo: ";
+    cin >> codigo;
+
+    for(int i = 0; i < roteiros.size(); i++){
+        if(roteiros[i].Codigo == codigo){
+            roteiros.erase(roteiros.begin() + i);
+            cout << "Roteiro excluido com sucesso!" << endl;
+            return;
+        }
+    }
+    cout << "Roteiro nao encontrado!" << endl;
+}
+
+void AlterarRoteiro(vector<Roteiro> &roteiros){
+    int codigo;
+    char opcao;
+    cout << "Digite o Codigo: ";
+    cin >> codigo;
+
+    for(int i = 0; i < roteiros.size(); i++){
+        if(roteiros[i].Codigo == codigo){
+            cout << "Codigo: " << roteiros[i].Codigo << endl;
+            cout << "Data e Hora: " << roteiros[i].Data_hora << endl;
+            cout << "Duracao: " << roteiros[i].Duracao << endl;
+            cout << "Origem: " << roteiros[i].Origem << endl;
+            cout << "Destino: " << roteiros[i].Destino << endl;
+            cout << "-------------------------------------" << endl;
+            cout << "Deseja alterar o Codigo? (S/N): ";
+            cin >> opcao;
+            if(opcao == 'S' || opcao == 's'){
+                cout << "Digite o Codigo: ";
+                cin >> roteiros[i].Codigo;
+            }
+            cout << "Deseja alterar a Data e Hora? (S/N): ";
+            cin >> opcao;
+            if(opcao == 'S' || opcao == 's'){
+                cout << "Digite a Data e Hora: ";
+                cin >> roteiros[i].Data_hora;
+            }
+            cout << "Deseja alterar a Duracao? (S/N): ";
+            cin >> opcao;
+            if(opcao == 'S' || opcao == 's'){
+                cout << "Digite a Duracao: ";
+                cin >> roteiros[i].Duracao;
+            }
+            cout << "Deseja alterar a Origem? (S/N): ";
+            cin >> opcao;
+            if(opcao == 'S' || opcao == 's'){
+                cout << "Digite a Origem: ";
+                cin >> roteiros[i].Origem;
+            }
+            cout << "Deseja alterar o Destino? (S/N): ";
+            cin >> opcao;
+            if(opcao == 'S' || opcao == 's'){
+                cout << "Digite o Destino: ";
+                cin >> roteiros[i].Destino;
+            }
+            cout << "Roteiro alterado com sucesso!" << endl;
+            return;
+        }
+    }
+    cout << "Roteiro nao encontrado!" << endl;
+}
+
+void ListarRoteiro(vector<Roteiro> &roteiros){
+    if(roteiros.size() == 0){
+        cout << "Nao ha roteiros cadastrados!" << endl;
+        return;
+    }
+    else{
+        for(int i = 0; i < roteiros.size(); i++){
+            cout << "Codigo: " << roteiros[i].Codigo << endl;
+            cout << "Data e Hora: " << roteiros[i].Data_hora << endl;
+            cout << "Duracao: " << roteiros[i].Duracao << endl;
+            cout << "Origem: " << roteiros[i].Origem << endl;
+            cout << "Destino: " << roteiros[i].Destino << endl;
+            cout << "-------------------------------------" << endl;
+        }
+    }
+}
+
+void LocalizarRoteiro(vector<Roteiro> &roteiros){
+    int codigo;
+    cout << "Digite o Codigo: ";
+    cin >> codigo;
+
+    for(int i = 0; i < roteiros.size(); i++){
+        if(roteiros[i].Codigo == codigo){
+            cout << "Codigo: " << roteiros[i].Codigo << endl;
+            cout << "Data e Hora: " << roteiros[i].Data_hora << endl;
+            cout << "Duracao: " << roteiros[i].Duracao << endl;
+            cout << "Origem: " << roteiros[i].Origem << endl;
+            cout << "Destino: " << roteiros[i].Destino << endl;
+            cout << "-------------------------------------" << endl;
+            return;
+        }
+    }
+    cout << "Roteiro nao encontrado!" << endl;
+}
+
